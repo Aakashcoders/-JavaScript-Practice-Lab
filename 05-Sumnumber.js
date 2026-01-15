522b78ab2f5ec17d0da193815d1f20552de46414
Aakash kumar <167094961+Aakashcoders@users.noreply.github.com>
2026-01-15 11:03:21 +0530

    Update 05-Sumnumber.js

--- 05-Sumnumber.js
+++ 05-Sumnumber.js
@@ -4,22 +4,7 @@ console.log("Sum Array")
 
 const arr = [15, 7, 8, -3, 9];
 let sum = 0;
-// if(arr.length < 0){
-//     // console.log(sum(...arr))
-//     console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4])
-// }
-// function sum(){
-//     console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4])
-// }
 
-
-// for(sum() of arr){
-//     console.log(sum)
-//     if(sum < 0){
-//         console.log(sum)
-//         break;
-//     }
-// }
 
 function sumnagtive(number){
     let sum = 0;
@@ -34,4 +19,5 @@ function sumnagtive(number){
 }
 
 const number  = [6, 8, 9, -2, 50]
+
 console.log(sumnagtive(number))
